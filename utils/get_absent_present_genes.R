2071b732026579dc0d62efbeaf56e3b1e09441db
daormar <dortiz@prhlt.upv.es>
2015-05-12 10:38:43 +0000

    Add two new scripts. Modified Files:  	utils/Makefile.am Added Files:  	utils/affy_to_eset.R utils/get_absent_present_genes.R

---
+++ utils/get_absent_present_genes.R
@@ -0,0 +1,69 @@
+# Author: Daniel Ortiz Mart\'inez
+# *- r -*
+
+# Basic libraries
+library(R.utils)
+
+# Set echo to TRUE
+# options(echo=TRUE) 
+
+## Collect arguments
+args <- commandArgs(asValue=TRUE, excludeReserved=TRUE)[-1]
+ 
+# Turn arguments into R variables
+keys <- attachLocally(args)
+#cat("Command-line arguments attached to global environment:\n");
+#print(keys);
+#str(mget(keys, envir=globalenv()))
+
+# Check arguments
+
+## Check --help option
+if("help" %in% keys || length(keys)==0)
+{    
+    cat("get_absent_present_genes [arguments]
+ 
+Arguments:
+-f     <string>        path to rda file
+-o     <string>        output directory
+--help                 print this text\n")
+  
+    q(save="no",status=0)
+}
+
+## Check -f option
+if(!"f" %in% keys)
+{
+    print("Error: -f option not provided")
+    q(save="no",status=1)
+}
+
+## Check -o option
+if(!"o" %in% keys)
+{
+    print("Error: -o option not provided")
+    q(save="no",status=1)
+}
+
+# Specific libraries
+library(Biobase)
+library(affy)
+library(gcrma)
+library(panp)
+
+# Process parameters
+
+## Load ExpressionSet file
+esetname=load(f)
+
+## Obtain highly and lowly expressed genes
+esetPaCalls <- pa.calls(get(esetname))
+paCalls <- esetPaCalls$Pcalls
+paVals <- esetPaCalls$Pvals
+paCalls <- ifelse(paCalls=="P","1",paCalls)
+paCalls <- ifelse(paCalls=="A","0",paCalls)
+paCalls <- ifelse(paCalls=="M","NA",paCalls)
+
+## Write result
+write.table(paCalls, file=paste(o,"hl_genes.csv",sep=""), sep=",", col.names=NA)
+#rm(list=ls())
