577bc57f64efcaeef005f2c76cab130f6144fdd7
daormar <dortiz@prhlt.upv.es>
2015-08-25 00:37:12 +0000

    Carry out FBA experiments for each sample.

--- utils/auto_fba.sh
+++ utils/auto_fba.sh
@@ -5,11 +5,10 @@
 function obtain_array_names()
 {
     # Initialize parameters
-    file=$1
-#    file=gse40595_subset_pdata.txt
+    _file=$1
 
     # Obtain array names
-    cat $file | $AWK -F "\"" '{
+    cat ${_file} | $AWK -F "\"" '{
                               if(NR>1)
                               {
                                 printf"%s\n",$2
@@ -21,13 +20,13 @@ function obtain_array_names()
 function obtain_array_types()
 {
     # Initialize parameters
-    file=$1
+    _file=$1
 
     # Obtain column number for "type" variable
-    coln=`head -1 ${file} | awk '{for(i=1;i<=NF;++i){if(index($i,"type")!=0) printf"%d",i}}'`
+    _coln=`head -1 ${file} | awk '{for(i=1;i<=NF;++i){if(index($i,"type")!=0) printf"%d",i}}'`
     
     # Obtain array types
-    cat $file | $AWK -F "\"" -v cn=${coln} '{
+    cat ${_file} | $AWK -F "\"" -v cn=${_coln} '{
                               if(NR>1)
                               {
                                 printf"%s\n",$(cn*3)
@@ -36,16 +35,48 @@ function obtain_array_types()
 }
 
 ########
-function obtain_array_of_type()
+function obtain_arrays_of_type()
 {
     # Initialize parameters
-    file=$1
-    type=$2
+    _file=$1
+    _type=$2
 
     # Obtain arrays of type
-    $GREP $type $file | $AWK -F "\"" '{
+    $GREP ${_type} ${_file} | $AWK -F "\"" '{
                                 printf"%s\n",$2
                              }'    
+}
+
+########
+function create_out_dir()
+{
+    _dir=$1
+    if [ ! -d ${_dir} ]; then
+        mkdir ${_dir} || { echo "Error while creating output directories: ${_dir}" >&2; exit 1; }
+    fi
+}
+
+########
+function fba_exp()
+{
+    # take parameters
+    _sample_file=$1
+    _exp_name=$2
+
+    # obtain absent/present genes
+    echo "** Obtaining absent/present genes..." >&2
+    echo "" >&2
+    $bindir/get_absent_present_genes -d  ${outd}/esetdir/esetgenes_to_entrezids.csv \
+        -p ${outd}/esetdir/panp_results_filt.csv -l ${_sample_file} > ${outd}/abs_pres_info/abs_pres_genes_filt_${_exp_name}.csv \
+        2>${outd}/abs_pres_info/get_absent_present_genes_${_exp_name}.log || exit 1
+
+    # generate linear programming problem in lp format
+    echo "** Generating linear programming problem in lp format..." >&2
+    echo "" >&2
+    $bindir/create_lp_file -s ${outd}/minfo/model -a ${outd}/abs_pres_info/abs_pres_genes_filt_${_exp_name}.csv \
+        -m ${outd}/esetdir/esetgenes_to_entrezids.csv -c 0 > ${outd}/lp/${_exp_name}.lp \
+        2> ${outd}/lp/create_lp_file_${_exp_name}.log || exit 1
+    echo "" >&2
 }
 
 ########
@@ -153,18 +184,14 @@ else
     # obtain model information
     echo "* Generating metabolic model information..." >&2
     echo "" >&2
-    if [ ! -d ${outd}/minfo ]; then
-        mkdir ${outd}/minfo || { echo "Error while creating output directories" >&2; exit 1; }
-    fi
-#    $bindir/extract_sbml_model_info -m $mfile -o ${outd}/minfo/model > ${outd}/minfo/extract_sbml_model_info.log 2>&1 || exit 1
+    create_out_dir ${outd}/minfo
+    $bindir/extract_sbml_model_info -m $mfile -o ${outd}/minfo/model > ${outd}/minfo/extract_sbml_model_info.log 2>&1 || exit 1
 
     # generate expression set
     echo "* Generating expression set..." >&2
     echo "" >&2
-    if [ ! -d ${outd}/esetdir ]; then
-        mkdir ${outd}/esetdir || { echo "Error while creating output directories" >&2; exit 1; }
-    fi
-#    $bindir/affy_to_eset -d $cdir -p $pfile -o ${outd}/esetdir/eset.rda > ${outd}/esetdir/affy_to_eset.log 2>&1 || exit 1
+    create_out_dir ${outd}/esetdir
+    $bindir/affy_to_eset -d $cdir -p $pfile -o ${outd}/esetdir/eset.rda > ${outd}/esetdir/affy_to_eset.log 2>&1 || exit 1
 
     # obtain entrezid's for genes
     echo "* Obtaining entrezid's for genes..." >&2
@@ -191,23 +218,29 @@ else
     $bindir/filter_panp_results -p ${outd}/esetdir/panp_results.csv \
         -g ${outd}/esetdir/${annot}_jscores.csv > ${outd}/esetdir/panp_results_filt.csv || exit 1
 
-    # obtain absent/present genes
-    echo "* Obtaining absent/present genes..." >&2
-    echo "" >&2
-    if [ ! -d ${outd}/abs_pres_info ]; then
-        mkdir ${outd}/abs_pres_info || { echo "Error while creating output directories" >&2; exit 1; }
-    fi
-    $bindir/get_absent_present_genes -d  ${outd}/esetdir/esetgenes_to_entrezids.csv \
-        -p ${outd}/esetdir/panp_results_filt.csv > ${outd}/abs_pres_info/abs_pres_genes_filt.csv \
-        2>${outd}/abs_pres_info/get_absent_present_genes.log || exit 1
+    ##########
 
-    # generate linear programming problem in lp format
-    echo "* Generating linear programming problem in lp format..." >&2
-    echo "" >&2
-    if [ ! -d ${outd}/lp ]; then
-        mkdir ${outd}/lp || { echo "Error while creating output directories" >&2; exit 1; }
-    fi
-    $bindir/create_lp_file -s ${outd}/minfo/model -a ${outd}/abs_pres_info/abs_pres_genes_filt.csv \
-        -m ${outd}/esetdir/esetgenes_to_entrezids.csv -c 0 > ${outd}/lp/fba.lp 2> ${outd}/lp/create_lp_file.log || exit 1
+    # Create directories required for the rest of the process
+
+    create_out_dir ${outd}/abs_pres_info
+    create_out_dir ${outd}/lp
+
+    ## Carry out an FBA experiment for all samples:
+
+    array_names=`obtain_array_names $pfile`
+
+    # Create temporary file
+    TMPFILE=`mktemp`
+    trap "rm -f $TMPFILE 2>/dev/null" EXIT
+
+    for arrn in ${array_names}; do
+
+        # write sample info to file
+        echo $arrn > $TMPFILE
+
+        # carry out fba experiment
+        fba_exp $TMPFILE $arrn
+
+    done
 
 fi
