625fe802a8e1378ece0f927468ef21ef6b56b93f
daormar <dortiz@prhlt.upv.es>
2015-12-07 13:23:02 +0000

    Add new functionality.

--- utils/plot_metatool_network.py
+++ utils/plot_metatool_network.py
@@ -6,9 +6,13 @@ import sys, getopt, fba
 
 ##################################################
 def print_help():
-    print >> sys.stderr, "plot_metatool_network -f <string> [--help]"
+    print >> sys.stderr, "plot_metatool_network -f <string> [-o] [-t <int>] [--help]"
     print >> sys.stderr, ""
     print >> sys.stderr, "-f <string> :    metatool model file" 
+    print >> sys.stderr, "-o          :    ommit arc label" 
+    print >> sys.stderr, "-t <int>    :    plot type"
+    print >> sys.stderr, "                 0 -> enzymes + metabolites (default option)"  
+    print >> sys.stderr, "                 1 -> enzymes + internal metabolites"  
     print >> sys.stderr, "--help      :    print this help message" 
     print >> sys.stderr, ""
 
@@ -19,20 +23,27 @@ class Metatool_info:
         self.enzirrev=set()
         self.metint=set()
         self.metext=set()
-        self.reactin={}
-        self.reactout={}
+        self.react_to_input_metab={}
+        self.react_to_output_metab={}
+        self.input_metab_to_react={}
+        self.output_metab_to_react={}
 
 ##################################################
 def process_cat_line(result,fields):
-    result.reactin[fields[0]]=[]
-    result.reactout[fields[0]]=[]
+    # Initialize variables for reaction
+    react=fields[0]
+    result.react_to_input_metab[react]={}
+    result.react_to_output_metab[react]={}
     cat_mode="in"
     i=2
+    
+    # Process entry
     while i<len(fields)-1:
         if(fields[i]=="="):
             cat_mode="out"
             i=i+1
         else:
+            # Process input metabolites
             if(cat_mode=="in"):
                 coef=1
                 if(fields[i]=="+"):
@@ -44,8 +55,12 @@ def process_cat_line(result,fields):
                     coef=coef*int(fields[i])
                     i=i+1
                 metab=fields[i]
-                result.reactin[fields[0]].append([coef,metab])
+                result.react_to_input_metab[react][metab]=coef
+                if(not metab in result.input_metab_to_react.keys()):
+                    result.input_metab_to_react[metab]={}
+                result.input_metab_to_react[metab][react]=coef
                 i=i+1
+            # Process output metabolites
             else:
                 coef=1
                 if(fields[i]=="+"):
@@ -57,7 +72,10 @@ def process_cat_line(result,fields):
                     coef=coef*int(fields[i])
                     i=i+1
                 metab=fields[i]
-                result.reactout[fields[0]].append([coef,metab])
+                result.react_to_output_metab[react][metab]=coef
+                if(not metab in result.output_metab_to_react.keys()):
+                    result.output_metab_to_react[metab]={}
+                result.output_metab_to_react[metab][react]=coef
                 i=i+1
 
 ##################################################
@@ -109,52 +127,77 @@ def load_metatool_file(metatoolf):
     return result
 
 ##################################################
-def print_fba_network(metatool_info):
+def print_enzymes_metab(metatool_info,int_metab_only,ommit_arc_label):
 
     # Print header
     print "digraph word_graph {"
     print "rankdir=LR;"
+    print "splines=ortho;"
+    print "K=2;"
 
     ## Set representation for the different nodes
 
     # Set representation for reactions
     print "node [shape = box]; ",
+#    print "node [shape = point]; ",
 
-    for key in metatool_info.reactin.keys():
+    for key in metatool_info.react_to_input_metab.keys():
         print key,";",
     print ""
 
     # Set representation for metabolites
     print "node [shape = circle];"
 
-    ## Process stochiometric relations
-    for (key,value) in metatool_info.reactin.iteritems():
-        for n in range(len(value)):
-            print value[n][1],"->",key, "[ label= \"",value[n][0],"\",","color = black ];"
+    ## Process stoichiometric relations
+    for (key,metabdict) in metatool_info.react_to_input_metab.iteritems():
+        for metab in metabdict.keys():
+            if(ommit_arc_label==0):
+                arc_label=metabdict[metab]
+            else:
+                arc_label=""
+            if(int_metab_only==0):
+                print metab,"->",key, "[ label= \"",arc_label,"\",","color = black ];"
+            else:
+                if(metab in metatool_info.metint):
+                    print metab,"->",key, "[ label= \"",arc_label,"\",","color = black ];"
 
-    for (key,value) in metatool_info.reactout.iteritems():
-        for n in range(len(value)):
-            print key,"->",value[n][1], "[ label= \"",value[n][0],"\",","color = black ];"
+    for (key,metabdict) in metatool_info.react_to_output_metab.iteritems():
+        for metab in metabdict.keys():
+            if(ommit_arc_label==0):
+                arc_label=metabdict[metab]
+            else:
+                arc_label=""
+            if(int_metab_only==0):
+                print key,"->",metab, "[ label= \"",arc_label,"\",","color = black ];"
+            else:
+                if(metab in metatool_info.metint):
+                    print key,"->",metab, "[ label= \"",arc_label,"\",","color = black ];"
 
     # Print footer
     print "}"
 
 ##################################################
-def plot_network(metatoolf):
+def plot_network(metatoolf,plottype,ommit_arc_label):
 
     # load metatoolf
     metatool_info=load_metatool_file(metatoolf)
 
     # print fba network
-    print_fba_network(metatool_info)
+    if(plottype==0):
+        print_enzymes_metab(metatool_info,0,ommit_arc_label)
+    elif(plottype==1):
+        print_enzymes_metab(metatool_info,1,ommit_arc_label)
 
 ##################################################
 def main(argv):
     # take parameters
     f_given=False
     metatoolf=""
+    t_given=0
+    plottype=0
+    ommit_arc_label=0
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hf:",["help","metatoolf="])
+        opts, args = getopt.getopt(sys.argv[1:],"hf:t:o",["help","metatoolf=","plottype=","ommit-arc-label"])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
@@ -169,6 +212,11 @@ def main(argv):
             elif opt in ("-f", "--metatoolf"):
                 metatoolf = arg
                 f_given=True
+            elif opt in ("-t", "--metatoolf"):
+                plottype= int(arg)
+                t_given=True
+            if opt in ("-o", "--ommit-arc-label"):
+                ommit_arc_label=1
 
     # print parameters
     if(f_given==True):
@@ -177,8 +225,11 @@ def main(argv):
         print >> sys.stderr, "Error: -f option not given"
         sys.exit(2)
 
+    if(t_given==True):
+        print >> sys.stderr, "t is %s" % (plottype)
+
     # create lp file according to selected criterion
-    plot_network(metatoolf)
+    plot_network(metatoolf,plottype,ommit_arc_label)
         
 if __name__ == "__main__":
     main(sys.argv)
